69639a49cbe0cb08e6684303e6667d53c4288ae7
Haisheng.Wu <freizl@gmail.com>
2016-10-17 09:15:41 -0700

    modify helm-do-grep to helm-do-ag since former one seems deprecated.

--- modules/prelude-helm.el
+++ modules/prelude-helm.el
@@ -56,7 +56,7 @@
 (global-unset-key (kbd "C-x c"))
 
 (define-key helm-command-map (kbd "o")     'helm-occur)
-(define-key helm-command-map (kbd "g")     'helm-do-grep)
+(define-key helm-command-map (kbd "g")     'helm-do-ag)
 (define-key helm-command-map (kbd "C-c w") 'helm-wikipedia-suggest)
 (define-key helm-command-map (kbd "SPC")   'helm-all-mark-rings)
 
